185695e9a6a0e681bc905c824acb0bffa55f9b14
Mahadeva K N <mahadeva_n@blr.trigent.com>
2019-07-25 13:42:04 +0530

    Updates

---
+++ precache-manifest.0e51de2aa20435f6dd681b71d8d18b9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638431d75e1cf706b3d2e9920a4175ea",
+    "url": "/my-tv/index.html"
+  },
+  {
+    "revision": "51620d7cb47b4bd13cea",
+    "url": "/my-tv/static/css/main.e907efa2.chunk.css"
+  },
+  {
+    "revision": "dc7230d011b1d40787e3",
+    "url": "/my-tv/static/js/2.fe8e32d7.chunk.js"
+  },
+  {
+    "revision": "51620d7cb47b4bd13cea",
+    "url": "/my-tv/static/js/main.066b047a.chunk.js"
+  },
+  {
+    "revision": "000cf35c3e18b3b5eabe",
+    "url": "/my-tv/static/js/runtime~main.25a808f2.js"
+  }
+]);
